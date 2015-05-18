f523801be09d1933fbad8e55174e27db94c73f50
hanchengen <1953804947@qq.com>
2015-05-18 14:53:55 +0800

    update index.php
    add instruction for index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+/**
+ *  应用入口文件 
+ */
+//框架入口文件
 $iweb = dirname(__FILE__)."/lib/iweb.php";
+//配置文件
 $config = dirname(__FILE__)."/config/config.php";
 require($iweb);
+//启动程序
 IWeb::createWebApp($config)->run();
 ?>
